e4d3ea4eaeba9480c34587f893a6c0072125ff1a
Victor Cui <thucjw@gmail.com>
2017-03-04 00:58:31 -0800

    Refactor the code to not rely on Twitter's internal source code but rely on public jars on Maven

--- server/scala/info/jcui/nicmonitor/MySQLClient.scala
+++ server/scala/info/jcui/nicmonitor/MySQLClient.scala
@@ -1,15 +1,17 @@
 package scala.info.jcui.nicmonitor
 
 
-import java.sql.ResultSet
-
-import com.twitter.querulous.evaluator.QueryEvaluator
-import com.twitter.querulous.query.QueryClass
+import java.sql.{ResultSet, DriverManager, Connection}
 
 /**
   * Created by jcui on 3/1/17.
   */
 class MySQLClient(host: String, username: String, password: String) {
+  Class.forName("com.mysql.jdbc.Driver")
+  val driver = "com.mysql.jdbc.Driver"
+  val url = "jdbc:mysql://" + host + ":3306/nic"
+  val connection: Connection = DriverManager.getConnection(url, username, password)
+  val statement = connection.createStatement()
 
   val createTableSql = """ CREATE TABLE IF NOT EXISTS nic_status
       (ts BIGINT NOT NULL,
@@ -19,30 +21,32 @@ class MySQLClient(host: String, username: String, password: String) {
       );
      """
 
-  //val createDbSql = """ CREATE DATABASE IF NOT EXISTS nic;"""
-  val queryEvaluator = QueryEvaluator(host, "nic", username, password)
-  val readQueryEvaluator = QueryEvaluator(host, "nic", username, password)
 
-  //queryEvaluator.execute(createDbSql)
-  //queryEvaluator.execute("use nic;")
-  queryEvaluator.execute(createTableSql)
+  statement.executeUpdate(createTableSql)
 
   def write(ts: Long, tx: Long, rx: Long): Unit = {
-    queryEvaluator.execute("INSERT INTO nic_status VALUES (?, ?, ?)", ts, tx, rx)
+    val sql = "INSERT INTO nic_status VALUES (%d, %d, %d)".format(ts, tx, rx)
+    statement.executeUpdate(sql)
+    Console.println("Writing to DB: " + sql)
   }
 
   def read(timeSpanInMinutes: Long, endTimeStamp: Long, byteType: String): Map[Long, Long] = {
     val startTimeStamp = endTimeStamp - timeSpanInMinutes * 60
     val selectQuery = "SELECT ts, %s FROM nic.nic_status WHERE ts > %d AND ts < %d;".format(byteType, startTimeStamp, endTimeStamp)
 
-    Console.println(s"Queury: $selectQuery")
+    Console.println(s"Reading to DB: $selectQuery")
 
-    val tsSizeTuples = readQueryEvaluator.select(selectQuery) {
-      r: ResultSet =>
-        (r.getLong(1), r.getLong(2))
+    val resultSet: ResultSet = statement.executeQuery(selectQuery)
+
+    val ret = new scala.collection.mutable.HashMap[Long, Long]
+
+    while(resultSet.next()) {
+      val ts = resultSet.getLong(1)
+      val data = resultSet.getLong(2)
+      ret += (ts -> data)
     }
 
-    tsSizeTuples.toMap
+    ret.toMap
   }
 
 }
